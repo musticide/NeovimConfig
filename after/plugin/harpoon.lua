8efa72729ef3005dfdbfddb74a297c9b9f9792cc
mustafa.khaizar <mustafa.khaizar@rampgroup.com>
2024-01-04 15:27:27 +0530

    harpoon fix

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,5 +4,5 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", function()mark.add_file()end)
 vim.keymap.set("n", "<C-e>", function()ui.toggle_quick_menu()end)
 
-vim.keymap.set("n", "<C-[>", function()ui.nav_prev()end)
-vim.keymap.set("n", "<C-]>", function()ui.nav_next()end)
+vim.keymap.set("n", "<PageUp>", function()ui.nav_prev()end)
+vim.keymap.set("n", "<PageDown>", function()ui.nav_next()end)
