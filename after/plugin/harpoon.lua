857e476512f91ee6dab1ceb2dd6bb3a3b14b0210
mustafa.khaizar <mustafa.khaizar@rampgroup.com>
2024-01-04 15:32:11 +0530

    harpoon log msg

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,9 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", function()mark.add_file()end)
+vim.keymap.set("n", "<leader>a", function()mark.add_file()
+print("File Harpooned")
+end)
 vim.keymap.set("n", "<C-e>", function()ui.toggle_quick_menu()end)
 
 vim.keymap.set("n", "<PageUp>", function()ui.nav_prev()end)
