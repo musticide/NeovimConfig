7f0c7f9128d64f8b01d2c00304556196e337e124
Mustafa K. Hussain <85333420+musticide@users.noreply.github.com>
2024-10-14 11:42:17 +0530

    Transparency calls

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,15 @@
 function ColorMyPencils(color)
 	--color = color or "onedark"
 	--vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+	
+	vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "MsgArea", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "NONE" })
 end
 
 ColorMyPencils()
