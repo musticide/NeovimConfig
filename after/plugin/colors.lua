c8f007fa7f89bf410d3dba80f498e0da2ba1022a
Mustafa K. Hussain <85333420+musticide@users.noreply.github.com>
2024-10-18 16:47:57 +0530

    Rose Pine Theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,85 +1,92 @@
-vim.cmd [[packadd packer.nvim]]
+require("rose-pine").setup({
+    variant = "moon", -- auto, main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    extend_background_behind_borders = false,
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use('nvim-lua/plenary.nvim')
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    -- use({
-    --     "rebelot/kanagawa.nvim",
-    --     as = 'kanagawa',
-    --     config = function()
-    --         vim.cmd('colorscheme kanagawa')
-    --     end
-    -- })
+    enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true, -- Handle deprecated options automatically
+    },
 
-    -- use({
-    --     "cpea2506/one_monokai.nvim",
-    --     config = function()
-    --         -- vim.cmd('colorscheme one_monokai')
-    --         require("one_monokai").setup({
-    --             transparent = true,
-    --             colors = {},
-    --             themes = function(colors)
-    --                 return {}
-    --             end,
-    --             italics = true,
-    --         })
-    --     end
-    -- })
+    styles = {
+        bold = true,
+        italic = false,
+        transparency = true,
+    },
 
-    use({
-        "navarasu/onedark.nvim",as = "onedark"
-    })
-    use { "rose-pine/neovim", as = "rose-pine" }
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment these if you want to manage the language servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
 
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            -- { 'rafamadriz/friendly-snippets' },
-            { 'Harry-Ross/vscode-c-snippets' },
-            { 'musticide/unity-snippets' },
-            { 'mfussenegger/nvim-lint' }
-        }
-    }
-    use('tpope/vim-fugitive')
-    use('lewis6991/gitsigns.nvim')
-    use('nvim-tree/nvim-web-devicons')
-    use('feline-nvim/feline.nvim')
-    use('NvChad/nvterm')
-    use('ThePrimeagen/harpoon')
-    use { 'kevinhwang91/nvim-ufo',
-        requires = {
-        use('kevinhwang91/promise-async')
-        }
-    }
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
 
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+    },
 
-    use('nvim-tree/nvim-tree.lua')
-    -- use('jose-elias-alvarez/null-ls.nvim')
-    use('numToStr/Comment.nvim')
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
-    use('kalvinpearce/ShaderHighlight')
-    use('rcarriga/nvim-notify')
-    use('chrisgrieser/nvim-rip-substitute')
-end)
+    palette = {
+        -- Override the builtin palette per variant
+        -- moon = {
+        --     base = '#18191a',
+        --     overlay = '#363738',
+        -- },
+    },
+
+    highlight_groups = {
+        -- Comment = { fg = "foam" },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+    },
+
+    before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end,
+})
+
+vim.cmd("colorscheme rose-pine")
+-- vim.cmd("colorscheme rose-pine-main")
+-- vim.cmd("colorscheme rose-pine-moon")
+-- vim.cmd("colorscheme rose-pine-dawn")
+function ColorMyPencils(color)
+	vim.cmd.colorscheme(color)
+	
+	vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "MsgArea", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
+    	vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "NONE" })
+end
+ColorMyPencils("rose-pine")
