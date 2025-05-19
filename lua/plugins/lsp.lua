9db99444da5348c5b806ec05e0bc46e73b5be5b8
musticide <mustafa25.tm@gmail.com>
2025-05-19 21:04:47 +0530

    disable copilot by default

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -471,6 +471,14 @@ return { -- Main LSP Configuration
 	},
 	{
 		"github/copilot.vim",
+		config = function()
+			vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+				expr = true,
+				replace_keycodes = false,
+			})
+			vim.g.copilot_no_tab_map = true
+			vim.cmd("Copilot disable")
+		end,
 	},
 	{
 		"kalvinpearce/ShaderHighlight",
