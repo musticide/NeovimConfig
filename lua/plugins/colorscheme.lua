8e838fcbfd037afbe8e97963649cfd581e8633cd
musticide <owlcity.musti@gmail.com>
2025-10-10 16:35:31 +0530

    Colorscheme: gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,199 +1,27 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		require("rose-pine").setup({
-			variant = "auto", -- auto, main, moon, or dawn
-			dark_variant = "main", -- main, moon, or dawn
-			dim_inactive_windows = false,
-			extend_background_behind_borders = true,
+		vim.g.gruvbox_material_foreground = "material" -- 'material', 'mix', 'original'
 
-			enable = {
-				terminal = true,
-				legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-				migrations = true, -- Handle deprecated options automatically
-			},
+		vim.g.gruvbox_material_enable_bold = 0
+		vim.g.gruvbox_material_enable_italic = true
+		vim.g.gruvbox_material_disable_italic_comment = 0 -- 0 is true
 
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = true,
-			},
+		vim.g.gruvbox_material_transparent_background = 2 -- 0: opaque, 1: transparent, 2: transparent + transparent UI
+		vim.g.gruvbox_material_ui_contrast = "high" -- 'low', 'high'
+		vim.g.gruvbox_material_dim_inactive_windows = 0
 
-			groups = {
-				border = "muted",
-				link = "iris",
-				panel = "surface",
+		vim.g.gruvbox_material_float_style = "bright" --`'bright'`, `'dim'`, `'none'`
 
-				error = "love",
-				hint = "iris",
-				info = "foam",
-				note = "pine",
-				todo = "rose",
-				warn = "gold",
+		vim.g.gruvbox_material_diagnostic_line_highlight = 0 -- 1 is true
+		vim.g.gruvbox_material_diagnostic_text_highlight = 0 -- 1 is true
 
-				git_add = "foam",
-				git_change = "rose",
-				git_delete = "love",
-				git_dirty = "rose",
-				git_ignore = "muted",
-				git_merge = "iris",
-				git_rename = "pine",
-				git_stage = "iris",
-				git_text = "rose",
-				git_untracked = "subtle",
-
-				h1 = "iris",
-				h2 = "foam",
-				h3 = "rose",
-				h4 = "gold",
-				h5 = "pine",
-				h6 = "foam",
-			},
-
-			palette = {
-				-- Override the builtin palette per variant
-				-- moon = {
-				--     base = '#18191a',
-				--     overlay = '#363738',
-				-- },
-			},
-
-			-- NOTE: Highlight groups are extended (merged) by default. Disable this
-			-- per group via `inherit = false`
-			highlight_groups = {
-				-- Comment = { fg = "foam" },
-				-- StatusLine = { fg = "love", bg = "love", blend = 15 },
-				-- VertSplit = { fg = "muted", bg = "muted" },
-				-- Visual = { fg = "base", bg = "text", inherit = false },
-			},
-
-			before_highlight = function(group, highlight, palette)
-				-- Disable all undercurls
-				-- if highlight.undercurl then
-				--     highlight.undercurl = false
-				-- end
-				--
-				-- Change palette colour
-				-- if highlight.fg == palette.pine then
-				--     highlight.fg = palette.foam
-				-- end
-			end,
-		})
-
-		-- vim.cmd("colorscheme rose-pine")
-		-- vim.cmd("colorscheme rose-pine-main")
-		vim.cmd("colorscheme rose-pine-moon")
-		-- vim.cmd("colorscheme rose-pine-dawn")
-	end,
-}
---[[ return {
-	"rebelot/kanagawa.nvim",
-	config = function()
-		require("kanagawa").setup({
-			compile = true, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = true },
-			functionStyle = {},
-			keywordStyle = { italic = true },
-			statementStyle = { bold = true },
-			typeStyle = {},
-			transparent = true, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = {
-					wave = {},
-					lotus = {},
-					dragon = {},
-					all = { ui = {
-						bg_gutter = "none",
-					} },
-				},
-			},
-			overrides = function(colors) -- add/modify highlights
-				local theme = colors.theme
-				return {
-					NormalFloat = { bg = "none" },
-					FloatBorder = { bg = "none" },
-					FloatTitle = { bg = "none" },
-
-					-- Save an hlgroup with dark background and dimmed foreground
-					-- so that you can use it where your still want darker windows.
-					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-					Cursorline = { bg = "#1a1a24" },
-
-					-- Popular plugins that open floats will link to NormalFloat by default;
-					-- set their background accordingly if you wish to keep them dark and borderless
-					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-					TelescopeTitle = { fg = theme.ui.special, bold = true },
-					TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-					TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-					TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-					TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-					TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-					TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-				}
-			end,
-			vim.cmd("colorscheme kanagawa-wave"),
-		})
-	end,
-	build = function()
-		vim.cmd("KanagawaCompile")
-	end,
-} ]]
---[[ return {
-	"navarasu/onedark.nvim",
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- Lua
-		require("onedark").setup({
-			-- Main options --
-			style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-			transparent = true, -- Show/hide background
-			term_colors = true, -- Change terminal color as per the selected theme style
-			ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-			cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-			-- toggle theme style ---
-			toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-			toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
-
-			-- Change code style ---
-			-- Options are italic, bold, underline, none
-			-- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-			code_style = {
-				comments = "italic",
-				keywords = "italic",
-				functions = "none",
-				strings = "none",
-				variables = "bold",
-			},
-
-			-- Lualine options --
-			lualine = {
-				transparent = false, -- lualine center bar transparency
-			},
-
-			-- Custom Highlights --
-			colors = {}, -- Override default colors
-			highlights = {}, -- Override highlight groups
-
-			-- Plugins Config --
-			diagnostics = {
-				darker = true, -- darker colors for diagnostic
-				undercurl = true, -- use undercurl instead of underline for diagnostics
-				background = true, -- use background color for virtual text
-			},
-		})
-
-		-- Enable theme
-		require("onedark").load()
+		vim.cmd.colorscheme("gruvbox-material")
 
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatTitle", { bg = "none" })
 	end,
-} ]]
+}
